aa34241b62397dc083169465780600ce3dfbdd5a
ophanim777 <rikyjo49@gmail.com>
2026-04-05 21:39:02 +0700

    add function to update data

--- cv-application/src/components/GeneralInfo.jsx
+++ cv-application/src/components/GeneralInfo.jsx
@@ -2,4 +2,9 @@ import { useState } from "react";
 
 export default function GeneralInfo({ data, setData }){
     const [isEditing, setIsEditing] = useState(true);
+
+    function handleChange(e) {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+}
 }
