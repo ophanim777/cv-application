c8c38528e78461ca41a41ce1f577d8c6e53fac03
ophanim777 <rikyjo49@gmail.com>
2026-04-05 22:08:25 +0700

    add main state

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -2,3 +2,9 @@ import { useState } from "react";
 import GeneralInfo from "./components/GeneralInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+
+function App() {
+  const [general, setGeneral] = useState({});
+  const [education, setEducation] = useState({});
+  const [experience, setExperience] = useState({});
+}
