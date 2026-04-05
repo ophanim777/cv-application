8208297361f312a6d9ab9bfdcea1357872db9c7d
ophanim777 <rikyjo49@gmail.com>
2026-04-05 22:09:25 +0700

    add return (ui)

--- cv-application/src/App.jsx
+++ cv-application/src/App.jsx
@@ -7,4 +7,15 @@ function App() {
   const [general, setGeneral] = useState({});
   const [education, setEducation] = useState({});
   const [experience, setExperience] = useState({});
+
+   return (
+    <div>
+      <h1>CV Application</h1>
+
+      <GeneralInfo data={general} setData={setGeneral} />
+      <Education data={education} setData={setEducation} />
+      <Experience data={experience} setData={setExperience} />
+    </div>
+  );
+
 }
