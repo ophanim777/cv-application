79e585e80f4a39a9f1f6d7ef33262253454d37b7
ophanim777 <rikyjo49@gmail.com>
2026-04-05 22:07:48 +0700

    import all function to app

---
+++ cv-application/src/App.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import GeneralInfo from "./components/GeneralInfo";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
